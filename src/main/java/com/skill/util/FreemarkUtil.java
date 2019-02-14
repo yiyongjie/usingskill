9f0a8b41e64bf37ee509582b10163ffd3ed032d1
yiyj479 <1074129195@qq.com>
2019-02-14 16:14:55 +0800

    改了下

--- src/main/java/com/skill/util/FreemarkUtil.java
+++ src/main/java/com/skill/util/FreemarkUtil.java
@@ -3,6 +3,7 @@ package com.skill.util;
 import com.skill.common.model.gen.GenContent;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -10,28 +11,32 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class FreemarkUtil {
-    private static final String MODEL_PATH = "src/main/java/com/skill/model";
-    private static final String MAPPER_PATH = "src/main/java/com/skill/dao";
-    private static final String XML_PATH = "src/main/resources/mapper";
 
-    public static void createData(String tableName){
+    public static void createData(){
         Configuration configuration = new Configuration();
         Writer out = null;
+        Properties prop = new Properties();
         try {
+            //获取生成的位置
+            prop.load(new FileInputStream(new File("src/main/resources/genurl.properties")));
             //获取模版路径
             Resource resource = new ClassPathResource("templates");
             File file = resource.getFile();
             configuration.setDirectoryForTemplateLoading(file);
             //获取到这个数据库里所有的表
             List<String> tableNames=DataUse.getTableNames();
+            String tableName=prop.getProperty("tableName");
             boolean isContain=tableNames.contains(tableName);
             if(isContain){
                 //准备一下模板参数
                 Map<String, Object> dataMap = new HashMap<String, Object>();
-                dataMap.put("modelClassPath", "com.skill.model");
-                dataMap.put("mapperClassPath", "com.skill.dao");
+//                dataMap.put("modelClassPath", "com.skill.model");
+//                dataMap.put("mapperClassPath", "com.skill.dao");
+                dataMap.put("modelClassPath", prop.getProperty("modelClassPath"));
+                dataMap.put("mapperClassPath", prop.getProperty("mapperClassPath"));
                 GenContent genContent=DataUse.getModel(tableName);
                 dataMap.put("genContent",genContent);
                 //  加载模版文件
@@ -39,9 +44,12 @@ public class FreemarkUtil {
                 Template mapperTemplate = configuration.getTemplate("mapper.ftl");
                 Template xmlTemplate = configuration.getTemplate("xml.ftl");
                 //生成数据位置,因拆多模块所以加模块，单模块不需要
-                String modelPosition=MODEL_PATH +"/"+genContent.getClassName()+".java";
-                String mapperPosition=MAPPER_PATH +"/"+genContent.getClassName()+"Mapper"+".java";
-                String xmlPosition=XML_PATH +"/"+genContent.getClassName()+".xml";
+//                String modelPosition=MODEL_PATH +"/"+genContent.getClassName()+".java";
+//                String mapperPosition=MAPPER_PATH +"/"+genContent.getClassName()+"Mapper"+".java";
+//                String xmlPosition=XML_PATH +"/"+genContent.getClassName()+".xml";
+                String modelPosition=prop.getProperty("MODEL_PATH") +"/"+genContent.getClassName()+".java";
+                String mapperPosition=prop.getProperty("MAPPER_PATH") +"/"+genContent.getClassName()+"Mapper"+".java";
+                String xmlPosition=prop.getProperty("XML_PATH") +"/"+genContent.getClassName()+".xml";
                 //输出model文件
                 out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(modelPosition))));
                 modelTemplate.process(dataMap, out);
@@ -68,6 +76,6 @@ public class FreemarkUtil {
     }
 
     public static void main(String[] args) {
-        createData("seckill_order");
+        createData();
     }
 }
