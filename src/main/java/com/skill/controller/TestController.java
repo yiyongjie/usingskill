5524e9eb651b2c9985e35517a4dbe2e6c7e46e33
yiyj479 <1074129195@qq.com>
2019-02-20 15:36:52 +0800

    改了下

--- src/main/java/com/skill/controller/TestController.java
+++ src/main/java/com/skill/controller/TestController.java
@@ -8,13 +8,16 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Api(value = "TestController",description = "测试用的玩意")
@@ -28,10 +31,30 @@ public class TestController {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-    @RequestMapping(value = "testone",method = RequestMethod.POST)
-    @ApiOperation(value = "测试数据库先")
-    public APIResponse test(@RequestBody SeckillOrder seckillOrder){
+    @RequestMapping(value = "testDB",method = RequestMethod.POST)
+    @ApiOperation(value = "测试数据库")
+    public APIResponse<List<SeckillOrder>> test(@RequestBody SeckillOrder seckillOrder){
         List<SeckillOrder> list=seckillOrderService.listSeckillOrder(seckillOrder);
         return APIResponse.success(list);
+    }
+
+    @RequestMapping(value = "testRedis",method = RequestMethod.GET)
+    @ApiOperation(value = "测试redis")
+    public APIResponse testRedis(){
+        APIResponse apiResponse=new APIResponse();
+        apiResponse.setCode("200");
+        apiResponse.setMsg("成功");
+        String a=stringRedisTemplate.opsForValue().get("name");
+        apiResponse.setData(a);
+        return apiResponse;
+    }
+
+    @RequestMapping(value = "testHttp",method = RequestMethod.GET)
+    @ApiOperation(value = "测试http")
+    public APIResponse testHttp(){
+        SeckillOrder seckillOrder=new SeckillOrder();
+        seckillOrder.setId(1L);
+        ResponseEntity<SeckillOrder> responseEntity=restTemplate.postForEntity("http://127.0.0.1:8080/test/testDB",seckillOrder,SeckillOrder.class);
+        return APIResponse.success(responseEntity.getBody());
     }
 }
