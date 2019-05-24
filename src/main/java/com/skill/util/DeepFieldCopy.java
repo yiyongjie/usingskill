54afa5e63d6b4a64bbf02d998af63a1a00296a97
yiyongjie <zevichen@qq.com>
2019-05-24 10:10:32 +0800

    继续

--- src/main/java/com/skill/util/DeepFieldCopy.java
+++ src/main/java/com/skill/util/DeepFieldCopy.java
@@ -2,11 +2,17 @@ package com.skill.util;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.List;
+
 /**
  * 对象转换工具
  */
 public abstract class DeepFieldCopy {
     public static <FromType, ToType> ToType transform(FromType sourceObject, Class<ToType> resultClass) {
         return JSON.parseObject(JSON.toJSONString(sourceObject), resultClass);
+    }
+
+    public static <FromType, ToType> List<ToType> transformList(List<FromType> sourceList, Class<ToType> resultClass) {
+        return JSON.parseArray(JSON.toJSONString(sourceList), resultClass);
     }
 }
